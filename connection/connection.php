22f6377b973bb56d8f342dc2a5869121a1b7ca1b
SeiryuJP <seiryujp7@gmail.com>
2022-10-12 14:04:37 +0200

    funcion login creada

--- connection/connection.php
+++ connection/connection.php
@@ -50,53 +50,25 @@
             return $player;
         }
 
-        // static function addPersona($id, $name, $phone){
-        //     self::$query = "INSERT INTO ".Credentials::$tablePersona." VALUES (?, ?, ?, ?)";
-        //     self::startConnection();
-        //     $stmt = self::$connection->prepare(self::$query);
-        //     $passwd = "";
-        //     $stmt->bind_param("ssss", $id, $name, $passwd, $phone);
-        //     if ($stmt->execute()){
-        //         self::$result = true;
-        //     }
-        //     else {
-        //         self::$result = false;
-        //     }
-        //     self::closeConnection();
-        //     return self::$result;
-        // }
+        static function login($name, $password){
+            self::$query = "SELECT * FROM ".Credentials::$tablePlayers." WHERE Name = ?";
+            self::startConnection();
+            $stmt = self::$connection->prepare(self::$query);
+            $stmt->bind_param("s", $name);
+            $stmt->execute();
+            self::$result = $stmt->get_result();
+            while ($row = self::$result->fetch_assoc()){
+                $player = new Player($row["Id"], $row["Name"], $row["Password"], $row["Wins"], $row["Losses"]);
+            }
+            self::$result->free_result();
+            self::closeConnection();
 
-        // static function editPersona($id, $name, $phone){
-        //     self::$query = "UPDATE ".Credentials::$tablePersona ." SET Nombre = ?, Clave = ?, Tfno = ? WHERE DNI = ?";
-        //     self::startConnection();
-        //     $stmt = self::$connection->prepare(self::$query);
-        //     $passwd = "";
-        //     $stmt->bind_param("ssss", $name, $passwd, $phone, $id);
-        //     $stmt->execute();
-        //     if ($stmt->affected_rows){
-        //         $edited = true;
-        //     }
-        //     else {
-        //         $edited = false;
-        //     }
-        //     self::closeConnection();
-        //     return $edited;
-        // }
-
-        // static function deletePersona($id){
-        //     self::$query = "DELETE FROM ".Credentials::$tablePersona." WHERE DNI = ?";
-        //     self::startConnection();
-        //     $stmt = self::$connection->prepare(self::$query);
-        //     $stmt->bind_param("s", $id);
-        //     $stmt->execute();
-        //     if ($stmt->affected_rows){
-        //         $deleted = true;
-        //     }
-        //     else {
-        //         $deleted = false;
-        //     }
-        //     self::closeConnection();
-        //     return $deleted;
-        // }
+            if ($player->getPassword() === $password){
+                return $player;
+            }
+            else {
+                return false;
+            }
+        }
     }
 ?>
--- index.php
+++ index.php
@@ -3,6 +3,8 @@
     include_once 'connection/connection.php';
 
     header("Content-Type:application/json");
+    $requestMethod = $_SERVER["REQUEST_METHOD"];
+    $paths = $_SERVER['REQUEST_URI'];
 
     $size = 10;
     $mines = 3;
@@ -11,5 +13,6 @@
     $field->putMines($mines);
 
     $players = Connection::getAllPlayers();
+    $result = Connection::login('admin', '1234');
 
-    echo json_encode($players);
+    echo json_encode($result);
