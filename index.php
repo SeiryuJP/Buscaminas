29c755452e73fc4812a97bab3eda48932d524310
SeiryuJP <seiryujp7@gmail.com>
2022-10-14 12:23:59 +0200

    index modificado

--- index.php
+++ index.php
@@ -2,112 +2,70 @@
     include_once 'class/field.php';
     include_once 'connection/connection.php';
 
-    session_start();
-
     header("Content-Type:application/json");
     $requestMethod = $_SERVER["REQUEST_METHOD"];
     $paths = $_SERVER['REQUEST_URI'];
 
-    // switch ($requestMethod) {
-    //     case 'GET':
-    //         $args = explode('/', $paths);
-    //         unset($args[0]);
-    //         if (empty($args[1]) && empty($args[2])){
-    //             $_SESSION['fieldClass'] = new Field(10);
-    //             $_SESSION['fieldClass']->putMines(3);
-    //             $_SESSION['hiddenField'] = array_fill(0, count($_SESSION['fieldClass']->getField()), '-');
+    switch ($requestMethod) {
+        case 'GET':
+            $args = explode('/', $paths);
+            unset($args[0]);
+            if (count($args) === 1){
+                $content = file_get_contents('php://input');
+                $data = json_decode($content, true);
+                $user = $data['name'];
+                if (empty($args[1])){
+                    header("HTTP/1.1 202 Invalid request");
+                    $message = [
+                        'cod' => '202',
+                        'desc' => 'Invalid number of arguments'
+                    ];
+                }
+                else {
+                    
+                }
+            }
+            elseif (count($args) === 2) {
+                if (empty($args[1]) || empty($args[2])){
+                    header("HTTP/1.1 202 Invalid request");
+                    $message = [
+                        'cod' => '202',
+                        'desc' => 'Invalid number of arguments'
+                    ];
+                }
+                else {
+                    $content = file_get_contents('php://input');
+                    $data = json_decode($content, true);
+                    $user = $data['name'];
+                    $size = $args[1];
+                    $mines = $args[2];
 
-    //             header("HTTP/1.1 200 Field created");
-    //             $message = [
-    //                 'cod' => '200',
-    //                 'field' => $_SESSION['hiddenField']
-    //             ];
+                    $field = new Field($user, $size);
+                    $field->putMines($mines);
 
-    //             $_SESSION['numberMines'] = 3;
-    //             $_SESSION['successCount'] = 0;
-    //             $_SESSION['loseStatus'] = false;
-    //             $_SESSION['winStatus'] = false;
-    //         }
-    //         elseif (empty($args[2])){
-    //             header("HTTP/1.1 202 Empty mines");
-    //             $message = [
-    //                 'cod' => '202',
-    //                 'desc' => 'Empty mines'
-    //             ];
-    //         }
-    //         else{
-    //             $_SESSION['fieldClass'] = new Field($args[1]);
-    //             $_SESSION['fieldClass']->putMines($args[2]);
-    //             $_SESSION['hiddenField'] = array_fill(0, count($_SESSION['fieldClass']->getField()), '-');
-
-    //             header("HTTP/1.1 200 Field created");
-    //             $message = [
-    //                 'cod' => '200',
-    //                 'field' => $_SESSION['hiddenField']
-    //             ];
-
-    //             $_SESSION['numberMines'] = $args[2];
-    //             $_SESSION['successCount'] = 0;
-    //             $_SESSION['loseStatus'] = false;
-    //             $_SESSION['winStatus'] = false;
-    //         }
-    //         break;
-
-    //     case 'POST':
-    //         $field = $_SESSION['fieldClass']->getField();
-    //         $args = explode('/', $paths);
-    //         unset($args[0]);
-    //         if (empty($args[1]) || $args[1] > count($field)){
-    //             header("HTTP/1.1 202 Invalid postion");
-    //             $message = [
-    //                 'cod' => '202',
-    //                 'desc' => 'invalid position',
-    //                 'field' => $_SESSION['hiddenField']
-    //             ];
-    //         }
-    //         else {
-    //             if (!$_SESSION['loseStatus'] && !$_SESSION['winStatus']){
-    //                 $position = $args[1];
-    //                 $_SESSION['hiddenField'][$position-1] = $field[$position-1];
-    //                 if ($_SESSION['hiddenField'][$position-1] === '*'){
-    //                     $_SESSION['loseStatus'] = true;
-                        
-    //                     header("HTTP/1.1 200 You lose");
-    //                     $message = [
-    //                         'cod' => '200',
-    //                         'field' => $_SESSION['hiddenField']
-    //                     ];
-    //                 }
-    //                 else {
-    //                     $_SESSION['successCount'] = $_SESSION['successCount'] + 1;
-    //                     if ($_SESSION['successCount'] === count($field) - $_SESSION['numberMines']){
-    //                         $_SESSION['winStatus'] = true;
-    //                     }
-
-    //                     header("HTTP/1.1 200 Mine avoided");
-    //                     $message = [
-    //                         'cod' => '200',
-    //                         'field' => $_SESSION['hiddenField']
-    //                     ];
-    //                 }
-    //             }
-    //             else {
-    //                 header("HTTP/1.1 200 New field needed");
-    //                 $message = [
-    //                     'cod' => '202',
-    //                     'desc' => 'create a new field'
-    //                 ];
-    //             } 
-    //         }
-    //         break;
-            
-    //     default:
-    //         header("HTTP/1.1 405 Invalid request");
-    //         $message = [
-    //             'cod' => '405',
-    //             'desc' => 'Invalid request'
-    //         ];
-    //         break;
-    // }
+                    header("HTTP/1.1 200 Field created");
+                    $message = [
+                        'cod' => '200',
+                        'field' => $field
+                    ];
+                }
+            }
+            else {
+                header("HTTP/1.1 202 Invalid request");
+                $message = [
+                    'cod' => '202',
+                    'desc' => 'Invalid number of arguments'
+                ];
+            }
+            break;
+        
+        default:
+            header("HTTP/1.1 405 Invalid request");
+            $message = [
+                'cod' => '405',
+                'desc' => 'Invalid request'
+            ];
+            break;
+    }
 
     echo json_encode($message);
